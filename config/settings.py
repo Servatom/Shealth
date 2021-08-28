c95a643262d4bb359b36d21d7207d37d7aa80d66
rdotjain <rupanshijain45678@gmail.com>
2021-08-28 20:45:39 +0530

    updated requirements & cors fix

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
